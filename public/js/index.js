abbb2faa9f735af89d55af9fd397381cac30c380
Eric Jacobs <eric@analog.sh>
2015-01-16 11:47:09 -0800

    Yet another map styling commit.

--- public/js/index.js
+++ public/js/index.js
@@ -7,7 +7,7 @@
   function draw() {
     var mapOpts = {
             center: new google.maps.LatLng(30,-50)
-          , zoom: 3
+          , zoom: 2
         }
       , map = new google.maps.Map(document.getElementById("map-canvas"), mapOpts)
       ;
